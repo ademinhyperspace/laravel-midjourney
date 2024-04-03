51b0ce30fea0b6e04b68c89833a87c05e2afdd93
ArthurMelikyan <arthmelikyan@gmail.com>
2024-04-03 10:08:04 +0400

    add new interface methods

--- src/Interfaces/LaraMidjourneyInterface.php
+++ src/Interfaces/LaraMidjourneyInterface.php
@@ -2,7 +2,14 @@
 
 namespace Arthmelikyan\Laramidjourney\Interfaces;
 
+use Arthmelikyan\Laramidjourney\DTO\GenerateImageDTO;
+use Arthmelikyan\Laramidjourney\DTO\ImageResourceDTO;
+
 interface LaraMidjourneyInterface
 {
-    public function generateImage(string $prompt);
+    public function generateImage(string $prompt): GenerateImageDTO;
+
+    public function findGeneratedImage(string $messageId): ImageResourceDTO;
+
+    public function imageToImage(string $imageFullUrl, string $prompt): GenerateImageDTO;
 }
